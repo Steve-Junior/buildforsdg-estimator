c2b17999cd405be1fa8d760575ff551234afafd8
Ibeh Stephen <steve.junior1230@gmail.com>
2020-04-13 12:53:18 +0000

    api ready for test 10

--- src/api/logs.php
+++ src/api/logs.php
@@ -11,7 +11,7 @@ require ('helper.php');
 $start   = microtime(true);
 $response = file_get_contents('activity.log');
 
-header("Content-type: text/html");
+header("Content-type: text/plain");
 http_response_code(200);
 
 echo $response;
